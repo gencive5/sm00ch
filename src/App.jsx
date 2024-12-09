93d5e2bf35016c943a5e08eeb3307ee4ee54bf92
gencive5 <vic.segen@gmail.com>
2024-12-09 17:41:12 +0100

    BASIC SET UP
    for starters :3

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>sm00ch!</h1>
+      <p>
+        TRy or PurchAse HERE
+      </p>
+    </div>
+  );
+}
+
+export default App;
