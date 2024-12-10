6eb30320a771b44592b134705de6a537398c67f7
gencive5 <vic.segen@gmail.com>
2024-12-10 18:41:25 +0100

    download button

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,7 @@ function App() {
 
   return (
     <div className="container mt-5">
-      <h1>sm00ch!?!!</h1>
-
-
+      <h1>sm00ch</h1>
 
       {/* Input Field */}
       <textarea
@@ -22,7 +20,23 @@ function App() {
 
       {/* Display Zone */}
       <div className="text-display">{userText}</div>
+
+    {/* Download Button */}
+    <div className="text-center mt-4">
+        <a
+          href="/sm00ch-font.woff" // Path to your font file in the public folder
+          download="sm00ch-font.woff" // Suggested download file name
+          className="btn btn-primary"
+        >
+          Download Font
+        </a>
+      </div>
+
+
     </div>
+
+    
+
   );
 }
 
