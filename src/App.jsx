66cec144aed0c49a0a3622e1f962318da61de8b9
gencive5 <vic.segen@gmail.com>
2024-12-13 13:14:01 +0100

    xtra styling for range slider
    needs xtra style +layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AnimatedFavicon from './animatedfavicon';
 
 function App() {
   const [userText, setUserText] = useState('');
-  const [fontSize, setFontSize] = useState(42); // Start at the middle of the range
+  const [fontSize, setFontSize] = useState(42); // Default font size, start in the middle
   const [fontColor, setFontColor] = useState('#000000');
 
   const firstInputRef = useRef(null);
@@ -24,36 +24,23 @@ function App() {
     <div className="container mt-5">
       <h1>sm00ch</h1>
 
+  
+      {/* Buttons */}
       <div className="button-row mb-3">
-        {/* Range Slider for Font Size Control */}
-        <div className="d-flex align-items-center">
+      <div className="custom-slider-container">
           <input
             type="range"
             min="12"
             max="72"
             value={fontSize}
             onChange={(e) => setFontSize(Number(e.target.value))}
-            className="form-range"
+            className="custom-slider"
           />
         </div>
-
-        {/* Buttons for Changing Font Color */}
-        <button
-          className="button-red ms-3"
-          onClick={() => changeFontColor('#B00F20')}>
-        </button>
-        <button
-          className="button-fuchsia"
-          onClick={() => changeFontColor('#99003F')}>
-        </button>
-        <button
-          className="button-pink"
-          onClick={() => changeFontColor('#B648AD')}>
-        </button>
-        <button
-          className="button-black"
-          onClick={() => changeFontColor('#000000')}>
-        </button>
+        <button className="button-red" onClick={() => changeFontColor('#B00F20')}></button>
+        <button className="button-fuchsia" onClick={() => changeFontColor('#99003F')}></button>
+        <button className="button-pink" onClick={() => changeFontColor('#B648AD')}></button>
+        <button className="button-black" onClick={() => changeFontColor('#000000')}></button>
       </div>
 
       {/* First Input Field */}
@@ -65,7 +52,7 @@ function App() {
         onChange={(e) => setUserText(e.target.value)}
       />
 
-      {/* Second Input Field (Interconnected) */}
+      {/* Second Input Field */}
       <textarea
         className="text-input2"
         placeholder="Start typing..."
@@ -76,11 +63,7 @@ function App() {
 
       {/* Download Button */}
       <div className="text-center mt-4">
-        <a
-          href="/sm00ch-font.woff"
-          download="sm00ch-font.woff"
-          className="btn-primary"
-        >
+        <a href="/sm00ch-font.woff" download="sm00ch-font.woff" className="btn-primary">
           DOWNLOAD FONT
         </a>
       </div>
