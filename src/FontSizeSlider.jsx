6da7478f4907f11adbd9efdf51aca9b958e08ef5
gencive5 <vic.segen@gmail.com>
2025-01-04 16:52:09 +0100

    more obvious font size slider
    + & -

--- src/FontSizeSlider.jsx
+++ src/FontSizeSlider.jsx
@@ -3,14 +3,18 @@ import React from 'react';
 function FontSizeSlider({ fontSize, setFontSize }) {
   return (
     <div className="custom-slider-container">
-      <input
-        type="range"
-        min="30"
-        max="150"
-        value={fontSize}
-        onChange={(e) => setFontSize(Number(e.target.value))}
-        className="custom-slider"
-      />
+      <span className="slider-symbol">-</span>
+      <div className="slider-wrapper">
+        <input
+          type="range"
+          min="30"
+          max="150"
+          value={fontSize}
+          onChange={(e) => setFontSize(Number(e.target.value))}
+          className="custom-slider"
+        />
+      </div>
+      <span className="slider-symbol">+</span>
     </div>
   );
 }
