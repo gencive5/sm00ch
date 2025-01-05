265573aeb8a4c340eb3772550201331199c46970
gencive5 <vic.segen@gmail.com>
2025-01-05 23:23:46 +0100

    updated npm and tried to solve react router error message
    is not too important, could be resolved with cache? check later

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import AnimatedFavicon from './Components/animatedfavicon';
+import AnimatedFavicon from './Components/AnimatedFavicon';
 import DownloadButton from './Components/DownloadButton';
-import DownloadButtonIOS from './Components/DownloadButtonIOS'; // New DownloadButton for iOS
+import DownloadButtonIOS from './Components/DownloadButtonIOS';
 import ColorButtons from './Components/ColorButtons';
 import FontSizeSlider from './Components/FontSizeSlider';
-import BlobPageIOS from './Components/BlobPageIOS'; // Page to display and download the blob
-import AndroidWarning from './Components/AndroidWarning'; // Import AndroidWarning component
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BlobPageIOS from './Components/BlobPageIOS';
+import AndroidWarning from './Components/AndroidWarning';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-function App() {
+const routes = [
+  {
+    path: '/',
+    element: (
+      <div className="container mt-5">
+        {/* Your components */}
+        <MainApp />
+      </div>
+    ),
+  },
+  {
+    path: '/blob-page',
+    element: <BlobPageIOS />,
+  },
+];
+
+// Add future flags to the router
+const router = createBrowserRouter(routes, {
+  future: {
+    v7_startTransition: true,
+    v7_relativeSplatPath: true,
+  },
+});
+
+function MainApp() {
   const [userText, setUserText] = useState('');
   const [fontSize, setFontSize] = useState(150);
   const [mobileFontSize] = useState(50);
@@ -72,129 +96,123 @@ function App() {
   const isAnyModalOpen = isModalOpen || isPlusMenuOpen;
 
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="container mt-5">
-              {isAndroidInAppBrowser && <AndroidWarning />}
-              <h1 className="title">sm00ch</h1>
-              <div className="button-row mb-3">
-                <FontSizeSlider fontSize={fontSize} setFontSize={setFontSize} />
-                <ColorButtons setFontColor={setFontColor} />
-              </div>
-              <textarea
-                ref={firstInputRef}
-                className="text-input"
-                placeholder="Start typing..."
-                value={userText}
-                onChange={(e) => setUserText(e.target.value)}
-                spellCheck={false}
+    <div>
+      {isAndroidInAppBrowser && <AndroidWarning />}
+      <h1 className="title">sm00ch</h1>
+      <div className="button-row mb-3">
+        <FontSizeSlider fontSize={fontSize} setFontSize={setFontSize} />
+        <ColorButtons setFontColor={setFontColor} />
+      </div>
+      <textarea
+        ref={firstInputRef}
+        className="text-input"
+        placeholder="Start typing..."
+        value={userText}
+        onChange={(e) => setUserText(e.target.value)}
+        spellCheck={false}
+      />
+      <textarea
+        className="text-input2"
+        placeholder="Start typing..."
+        value={userText}
+        onChange={(e) => setUserText(e.target.value)}
+        style={{ fontSize: `${fontSize}px`, color: fontColor }}
+        spellCheck={false}
+      />
+      <div className="button-row row2 mb-3 text-center mt-4">
+        {!isPlusMenuOpen && (
+          <>
+            {isIOSInstagram ? (
+              <DownloadButtonIOS
+                userText={userText}
+                fontSize={fontSize}
+                fontColor={fontColor}
+                showPlusButton={showPlusButton}
               />
-              <textarea
-                className="text-input2"
-                placeholder="Start typing..."
-                value={userText}
-                onChange={(e) => setUserText(e.target.value)}
-                style={{ fontSize: `${fontSize}px`, color: fontColor }}
-                spellCheck={false}
+            ) : (
+              <DownloadButton
+                userText={userText}
+                fontSize={fontSize}
+                fontColor={fontColor}
+                showPlusButton={showPlusButton}
               />
-              <div className="button-row row2 mb-3 text-center mt-4">
-                {!isPlusMenuOpen && (
-                  <>
-                    {isIOSInstagram ? (
-                      <DownloadButtonIOS
-                        userText={userText}
-                        fontSize={fontSize}
-                        fontColor={fontColor}
-                        showPlusButton={showPlusButton}
-                      />
-                    ) : (
-                      <DownloadButton
-                        userText={userText}
-                        fontSize={fontSize}
-                        fontColor={fontColor}
-                        showPlusButton={showPlusButton}
-                      />
-                    )}
-                  </>
-                )}
-                {showPlusButton ? (
-                  <>
-                    <button className="bttn btn-plus" onClick={togglePlusMenu}>
-                      {isAnyModalOpen ? 'X' : '+'}
-                    </button>
-                    {isPlusMenuOpen && (
-                      <div className="plus-menu-modal">
-                        <a href="/sm00ch.zip" download="sm00ch.zip" className="bttn btn-downloaf">
-                          DOWNLOAD FONT
-                        </a>
-                        <button className="bttn btn-use" onClick={toggleModal}>
-                          USE CONDITIONS
-                        </button>
-                        <a
-                          href="https://www.paypal.me/VicSegen?locale.x=fr_FR"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <button className="bttn btn-donate">DONATE</button>
-                        </a>
-                        <a
-                          href="https://youtu.be/QgW_smQAwyE"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <button className="bttn btn-2">sm00ch 2</button>
-                        </a>
-                      </div>
-                    )}
-                  </>
-                ) : (
-                  <>
-                    <button className="bttn btn-use" onClick={toggleModal}>
-                      {isModalOpen ? 'X' : 'USE CONDITIONS'}
-                    </button>
-                    <a
-                      href="https://www.paypal.me/VicSegen?locale.x=fr_FR"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <button className="bttn btn-donate">DONATE</button>
-                    </a>
-                    <a
-                      href="https://youtu.be/QgW_smQAwyE"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <button className="bttn btn-2">sm00ch 2</button>
-                    </a>
-                  </>
-                )}
+            )}
+          </>
+        )}
+        {showPlusButton ? (
+          <>
+            <button className="bttn btn-plus" onClick={togglePlusMenu}>
+              {isAnyModalOpen ? 'X' : '+'}
+            </button>
+            {isPlusMenuOpen && (
+              <div className="plus-menu-modal">
+                <a href="/sm00ch.zip" download="sm00ch.zip" className="bttn btn-downloaf">
+                  DOWNLOAD FONT
+                </a>
+                <button className="bttn btn-use" onClick={toggleModal}>
+                  USE CONDITIONS
+                </button>
+                <a
+                  href="https://www.paypal.me/VicSegen?locale.x=fr_FR"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="bttn btn-donate">DONATE</button>
+                </a>
+                <a
+                  href="https://youtu.be/QgW_smQAwyE"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="bttn btn-2">sm00ch 2</button>
+                </a>
               </div>
-              {isModalOpen && (
-                <div className="modal-overlay" onClick={toggleModal}>
-                  <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                    <p>
-                      THIS FONT IS AVAILABLE FOR PERSONAL USE ONLY.
-                      <br />
-                      CREDIT GENCIVE5 ON INSTAGRAM.
-                      <br />
-                      FOR ANY COMMERCIAL USE PLEASE CONTACT ME AT @GENCIVE5 OR VIC.SEGEN@GMAIL.COM
-                      <br />
-                      YOU CAN DONATE IF YOU WANT.
-                    </p>
-                  </div>
-                </div>
-              )}
-              <AnimatedFavicon />
-            </div>
-          }
-        />
-        <Route path="/blob-page" element={<BlobPageIOS />} />
-      </Routes>
-    </Router>
+            )}
+          </>
+        ) : (
+          <>
+            <button className="bttn btn-use" onClick={toggleModal}>
+              {isModalOpen ? 'X' : 'USE CONDITIONS'}
+            </button>
+            <a
+              href="https://www.paypal.me/VicSegen?locale.x=fr_FR"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="bttn btn-donate">DONATE</button>
+            </a>
+            <a
+              href="https://youtu.be/QgW_smQAwyE"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="bttn btn-2">sm00ch 2</button>
+            </a>
+          </>
+        )}
+      </div>
+      {isModalOpen && (
+        <div className="modal-overlay" onClick={toggleModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <p>
+              THIS FONT IS AVAILABLE FOR PERSONAL USE ONLY.
+              <br />
+              CREDIT GENCIVE5 ON INSTAGRAM.
+              <br />
+              FOR ANY COMMERCIAL USE PLEASE CONTACT ME AT @GENCIVE5 OR VIC.SEGEN@GMAIL.COM
+              <br />
+              YOU CAN DONATE IF YOU WANT.
+            </p>
+          </div>
+        </div>
+      )}
+      <AnimatedFavicon />
+    </div>
   );
+}
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
